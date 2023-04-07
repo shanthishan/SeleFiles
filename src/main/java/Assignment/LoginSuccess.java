c41239fbcc2cd4baec8d7588bfd129b17096c505
shanthishan <shanthi.viji66@gmail.com>
2023-04-08 00:10:52 +0530

    initial commit

---
+++ src/main/java/Assignment/LoginSuccess.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginSuccess extends BaseClass {
+    @Test
+    public void test() throws InterruptedException {
+
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.name("username")).sendKeys("tomsmith");
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//button[@class='radius']")).click();
+        WebElement text=driver.findElement(By.id("flash"));
+        System.out.println(text.getText());
+        Assert.assertEquals(text.getText(),"You logged into a secure area!\n" +
+                "×");
+
+        Thread.sleep(2000);
+    }
+}
